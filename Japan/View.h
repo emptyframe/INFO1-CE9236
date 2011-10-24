991d8c6d9c497e293193e97916368bcb6d0ab32b
Calvin CHOI <beatmania3rd@yahoo.com>
2011-10-24 01:32:26 -0400

    Initialize

---
+++ Japan/View.h
@@ -0,0 +1,16 @@
+//
+//  View.h
+//  Japan
+//
+//  Created by Calvin CHOI on 10/24/11.
+//  Copyright 2011 Comedy Central. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface View : UIView {
+    
+}
+
+@end
