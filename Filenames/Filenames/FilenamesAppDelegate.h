a3993105a396606ac52d7e9dd59078f1e8596d69
Calvin CHOI <beatmania3rd@yahoo.com>
2011-11-28 18:09:10 -0500

    1st commit

---
+++ Filenames/Filenames/FilenamesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FilenamesAppDelegate.h
+//  Filenames
+//
+//  Created by Calvin CHOI on 11/28/11.
+//  Copyright (c) 2011 Comedy Central. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilenamesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Filenames/Filenames/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Filenames
+//
+//  Created by Calvin CHOI on 11/28/11.
+//  Copyright (c) 2011 Comedy Central. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FilenamesAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FilenamesAppDelegate class]));
+    }
+}
